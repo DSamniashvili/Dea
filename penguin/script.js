04891a2717d3df2f0eeab5d1147c41873318d5f7
DSamniashvili <38917781+DSamniashvili@users.noreply.github.com>
2018-10-07 21:15:27 +0400

    Add files via upload

---
+++ penguin/script.js
@@ -0,0 +1,14 @@
+function toggleFunc(e) {
+
+    e.classList.toggle('show');
+
+    let elem = document.getElementById('navID'),
+        Style = window.getComputedStyle(elem),
+        right = Style.getPropertyValue('right');
+
+    if (right == '0px') {
+        elem.style.right = "-260px";
+    } else {
+        elem.style.right = "0px";
+    }
+}
