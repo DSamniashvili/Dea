d628d28c2884c4c3305251148b25bfc1d3562e9b
DSamniashvili <38917781+DSamniashvili@users.noreply.github.com>
2018-11-13 18:51:17 +0400

    CSS-JS update

--- november_11/november_11/scripts/script.js
+++ november_11/november_11/scripts/script.js
@@ -2,22 +2,33 @@
 
 let slideshow = document.getElementById('slideshow')
 
+
 class Slideshow {
-    constructor(n, width, height) {
+    constructor(n, width, height, textArr) {
         this.n = n;
         this.width = width;
         this.height = height;
+        this.textArr = textArr;
     }
     generateSlideshow() {
         for (let i = 0; i < this.n; i++) {
             let slide = document.createElement('div');
+            let text = document.createElement('h1');
+
             slide.style.width = this.width;
             slide.style.height = this.height;
-            slide.className = `slide-element slide=${i}`;
+            slide.className = `slide-element slide-${i}`;
+
+
+            text.className = ` slide-text slide-text-${i}`;
+            text.textContent = this.textArr[0];
+            this.textArr.shift();
+
+            slide.appendChild(text);
             slideshow.appendChild(slide);
         }
     }
 }
 
-let slideS = new Slideshow(4, 300, 200);
+let slideS = new Slideshow(4, 300, 200, ['Bankok', 'Singapure', 'Tbilisi', 'Khashuri']);
 slideS.generateSlideshow();
