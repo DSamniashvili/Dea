2ef000ac7ec68eaa40becc9fda0644f15901426f
DSamniashvili <deasamniashvili@gmail.com>
2018-12-02 12:06:43 +0400

    built2

---
+++ site_2/react-site2/build/precache-manifest.4f0d9a960891dd74afccc4d14d05f2c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5a7c5dbca16ef5ac2438",
+    "url": "/static/js/main.5a7c5dbc.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "5a7c5dbca16ef5ac2438",
+    "url": "/static/css/main.c9a47566.chunk.css"
+  },
+  {
+    "revision": "15b0cccc2569486f09ced74d2cb1de34",
+    "url": "/index.html"
+  }
+];
